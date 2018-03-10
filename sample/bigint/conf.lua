9732e2d49b8ead21dc873aaa1d498617b359713f
Xiongfei Shi <jenson.shixf@gmail.com>
2018-03-10 23:33:47 +0800

    bigint示例

---
+++ sample/bigint/conf.lua
@@ -0,0 +1,11 @@
+------------------------------------------------------------
+--  bigint/conf.lua
+--
+--  copyright (c) 2018 Xiongfei Shi
+--
+--  author: Xiongfei Shi <jenson.shixf(a)gmail.com>
+--  license: Apache-2.0
+------------------------------------------------------------
+
+function sevo.conf(c)
+end
