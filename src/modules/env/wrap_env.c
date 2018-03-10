45cf3b594d397439947fede92be2128e0d2b2113
Xiongfei Shi <jenson.shixf@gmail.com>
2018-03-10 17:09:28 +0800

    线程安全的ID生成器 & htable放到核心库

--- src/modules/env/wrap_env.c
+++ src/modules/env/wrap_env.c
@@ -8,22 +8,21 @@
 */
 
 #include "wrap_env.h"
-#include "common/htable.h"
 
-static htable_t     *env_htable = NULL;
+static mc_htable_t  *env_htable = NULL;
 static mc_rwlock_t  env_rwlock = { 0, 0 };
 
 int env_init(void) {
     if (!env_htable) {
         mc_rwlock_create(&env_rwlock);
-        env_htable = htable_create();
+        env_htable = mc_htable_create();
     }
     return 0;
 }
 
 void env_deinit(void) {
     if (env_htable) {
-        htable_destroy(env_htable);
+        mc_htable_destroy(env_htable);
         env_htable = NULL;
     }
     mc_rwlock_destroy(&env_rwlock);
@@ -36,7 +35,7 @@ static int env_ht_add(lua_State * L) {
     int retval;
 
     mc_rwlock_wrlock(&env_rwlock);
-    retval = htable_add(env_htable, k, (int)kl, v, (int)vl);
+    retval = mc_htable_add(env_htable, k, (int)kl, v, (int)vl);
     mc_rwlock_wrunlock(&env_rwlock);
 
     lua_pushboolean(L, 0 == retval);
@@ -50,7 +49,7 @@ static int env_ht_set(lua_State * L) {
     int retval;
 
     mc_rwlock_wrlock(&env_rwlock);
-    retval = htable_set(env_htable, k, (int)kl, v, (int)vl);
+    retval = mc_htable_set(env_htable, k, (int)kl, v, (int)vl);
     mc_rwlock_wrunlock(&env_rwlock);
 
     lua_pushboolean(L, 0 == retval);
@@ -60,13 +59,13 @@ static int env_ht_set(lua_State * L) {
 static int env_ht_get(lua_State * L) {
     size_t l = 0;
     const char *k = luaL_checklstring(L, 1, &l);
-    const datum_t *dt;
+    const mc_datum_t *dt;
 
     mc_rwlock_rdlock(&env_rwlock);
-    dt = htable_get(env_htable, k, (int)l);
+    dt = mc_htable_get(env_htable, k, (int)l);
     if (dt) {
         int vl = 0;
-        const char *v = datum_value(dt, &vl);
+        const char *v = (const char *)mc_datum_value(dt, &vl);
         lua_pushlstring(L, v, vl);
     } else {
         lua_pushnil(L);
@@ -82,7 +81,7 @@ static int env_ht_del(lua_State * L) {
     int retval;
 
     mc_rwlock_wrlock(&env_rwlock);
-    retval = htable_erase(env_htable, k, (int)l);
+    retval = mc_htable_erase(env_htable, k, (int)l);
     mc_rwlock_wrunlock(&env_rwlock);
 
     lua_pushboolean(L, 0 == retval);
@@ -91,23 +90,23 @@ static int env_ht_del(lua_State * L) {
 
 static int env_ht_keys(lua_State * L) {
     lua_Integer idx = 1;
-    htable_iter_t *iter = htable_iter(env_htable);
-    const datum_t *dt;
+    mc_htable_iter_t *iter = mc_htable_iter(env_htable);
+    const mc_datum_t *dt;
     const char *k;
     int l;
 
     lua_newtable(L);
 
     mc_rwlock_rdlock(&env_rwlock);
-    while (!!(dt = htable_iter_next(iter))) {
-        k = datum_key(dt, &l);
+    while (!!(dt = mc_htable_iter_next(iter))) {
+        k = (const char *)mc_datum_key(dt, &l);
 
         lua_pushlstring(L, k, l);
         lua_seti(L, -2, idx++);
     }
     mc_rwlock_rdunlock(&env_rwlock);
 
-    htable_iter_destroy(iter);
+    mc_htable_iter_destroy(iter);
 
     return 1;
 }
--- src/modules/id/wrap_id.c
+++ src/modules/id/wrap_id.c
@@ -10,11 +10,8 @@
 #include "wrap_id.h"
 #include "modules/gmp/wrap_gmp.h"
 
-static mc_id_t g_idgen;
-static unsigned long long ID_EPOCH = 1483228800000ULL;  /* 2017-01-01 00:00:00 (UTC) */
-
 static int mcl_id_init(lua_State * L) {
-    unsigned long long epoch = ID_EPOCH;
+    long long epoch = -1;
     int nodeid, top;
     mpz_t ep;
 
@@ -25,25 +22,25 @@ static int mcl_id_init(lua_State * L) {
         mpz_init(ep);
 
         luaX_checkmpz(L, 2, ep);
-        epoch = mpz_get_ull(ep);
+        epoch = mpz_get_ll(ep);
 
         mpz_clear(ep);
     }
 
-    mc_id_init(&g_idgen, epoch, nodeid);
+    mc_id_set(epoch, nodeid);
 
     return 0;
 }
 
 static int mcl_id_next(lua_State * L) {
     bigint_t *bi = new_bigint(L);
-    mpz_set_ull(bi->x, mc_id(&g_idgen, NULL));
+    mpz_set_ll(bi->x, mc_id(NULL));
     return 1;
 }
 
 static int mcl_id_split(lua_State * L) {
     bigint_t *bi = new_bigint(L);
-    unsigned long long timestamp;
+    long long timestamp;
     int sequence, nodeid;
     mpz_t id;
 
@@ -51,7 +48,7 @@ static int mcl_id_split(lua_State * L) {
 
     luaX_checkmpz(L, 1, id);
 
-    mc_id_split(&g_idgen, mpz_get_ull(id), &timestamp, &nodeid, &sequence);
+    mc_id_split(mpz_get_ll(id), &timestamp, &nodeid, &sequence);
 
     mpz_set_ull(bi->x, timestamp);
 
@@ -71,7 +68,6 @@ int luaopen_sevo_id(lua_State* L) {
         { NULL, NULL }
     };
 
-    mc_id_init(&g_idgen, ID_EPOCH, -1);
     luaX_register_module(L, "id", mod_id);
 
     return 0;
